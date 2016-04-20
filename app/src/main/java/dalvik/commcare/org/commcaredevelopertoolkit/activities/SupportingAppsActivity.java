e5f192a0ea47126c3531e096347240f4f1a26ae7
ALIZA STONE <amstone326@gmail.com>
2016-04-20 17:44:13 -0400

    supporting apps utility done besides icon

--- app/src/main/java/dalvik/commcare/org/commcaredevelopertoolkit/activities/SupportingAppsActivity.java
+++ app/src/main/java/dalvik/commcare/org/commcaredevelopertoolkit/activities/SupportingAppsActivity.java
@@ -1,9 +1,8 @@
 package dalvik.commcare.org.commcaredevelopertoolkit.activities;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.text.Layout;
-import android.text.util.Linkify;
 import android.view.LayoutInflater;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -21,26 +20,47 @@ public class SupportingAppsActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.supporting_apps_layout);
-        fillInAppLinks();
+        if (playStoreIsInstalled()) {
+            setContentView(R.layout.supporting_apps_layout);
+            fillInAppLinks();
+        } else {
+            setContentView(R.layout.supporting_apps_unavailable_layout);
+        }
+    }
+
+    private boolean playStoreIsInstalled() {
+        PackageManager pm = getPackageManager();
+        try {
+            pm.getPackageInfo("com.android.vending", PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
     }
 
     private void fillInAppLinks() {
         HashMap<String,String> appNamesToLinks = new HashMap<>();
-        appNamesToLinks.put("ES File Explorer:", "https://play.google.com/store/apps/details?id=com.estrongs.android.pop");
-        appNamesToLinks.put("Barcode Scanner:", "https://play.google.com/store/apps/details?id=com.androidrocker.qrscanner");
+        appNamesToLinks.put("ES File Explorer", "https://play.google.com/store/apps/details?id=com.estrongs.android.pop");
+        appNamesToLinks.put("QR Code Scanner", "https://play.google.com/store/apps/details?id=com.androidrocker.qrscanner");
+        appNamesToLinks.put("Audio Recorder", "https://play.google.com/store/apps/details?id=dje073.android.audiorecorderlite");
+        appNamesToLinks.put("Breath Counter", "https://play.google.com/store/apps/details?id=org.commcare.respiratory");
+        appNamesToLinks.put("Area Mapper", "https://play.google.com/store/apps/details?id=richard.chard.lu.android.areamapper");
 
         TableLayout table = (TableLayout)findViewById(R.id.app_links_table);
+        int index = 0;
         for (String appName : appNamesToLinks.keySet()) {
-            addRow(table, appName, appNamesToLinks.get(appName));
+            addRow(table, appName, appNamesToLinks.get(appName), index);
+            index++;
         }
     }
 
-    private void addRow(TableLayout table, String appName, String appLink) {
-        TableLayout rootTable = (TableLayout) LayoutInflater.from(this).inflate(R.layout.supporting_app_row_view, table);
-        RowView row = rootTable.findViewById()
-        TextView appNameView = (TextView)rootTable.findViewById(R.id.app_name_view);
-        TextView appLinkView = (TextView)rootTable.findViewById(R.id.app_link_view);
+    private void addRow(TableLayout table, String appName, String appLink, int index) {
+        TableLayout rootTable = (TableLayout)LayoutInflater.from(this).inflate(R.layout.supporting_app_row_view, table);
+        TableRow row = (TableRow)rootTable.findViewById(R.id.row_view);
+        row.setId(index);
+
+        TextView appNameView = (TextView)row.findViewById(R.id.app_name_view);
+        TextView appLinkView = (TextView)row.findViewById(R.id.app_link_view);
         appNameView.setText(appName);
         appLinkView.setText(appLink);
     }
